93db9b4ba34f6ed32e42125805af3d698d812107
cristina07a <cristinatom2002@gmail.com>
2024-11-25 19:16:36 +0200

    Git LFS track
    .py, .lib, .dnnl extensions

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import torch
-
-
-data = [1,2,3,4,5]
-
-tensor = torch.tensor(data)
-
-print(tensor)
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5a52e73a35b98cb9d48e6406b4e42341eccca2444f90c43727acbe19882b99a
+size 84
